d617e8dc0dbc757aea17aa7c1148749b5bd421c0
Lukas Kahwe Smith <lukas.smith@witty.works>
2021-11-29 21:11:09 +0100

    typo fix

--- app/Providers/JetstreamServiceProvider.php
+++ app/Providers/JetstreamServiceProvider.php
@@ -51,7 +51,7 @@ class JetstreamServiceProvider extends ServiceProvider
     {
         Jetstream::defaultApiTokenPermissions(['read']);
 
-        Jetstream::role('admin', _('content.admin'), [
+        Jetstream::role('admin', __('content.admin'), [
             'create',
             'read',
             'update',
