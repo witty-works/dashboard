604a39b9dc122bb9323e74f2a4652d72562e46c7
Lukas Kahwe Smith <lukas.smith@witty.works>
2021-11-29 20:27:38 +0100

    customize country list

---
+++ app/Spark/Countries.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Spark;
+
+class Countries
+{
+    /**
+     * Get all of the countries in the world.
+     *
+     * @return array
+     */
+    public static function all()
+    {
+        return [
+            'AL' => __('Albania'),
+            'AD' => __('Andorra'),
+            'AM' => __('Armenia'),
+            'AU' => __('Australia'),
+            'AT' => __('Austria'),
+            'BE' => __('Belgium'),
+            'BG' => __('Bulgaria'),
+            'CA' => __('Canada'),
+            'HR' => __('Croatia'),
+            'CY' => __('Cyprus'),
+            'CZ' => __('Czech Republic'),
+            'DK' => __('Denmark'),
+            'EE' => __('Estonia'),
+            'FO' => __('Faroe Islands'),
+            'FI' => __('Finland'),
+            'FR' => __('France'),
+            'DE' => __('Germany'),
+            'GI' => __('Gibraltar'),
+            'GR' => __('Greece'),
+            'VA' => __('Holy See (Vatican City State)'),
+            'HU' => __('Hungary'),
+            'IS' => __('Iceland'),
+            'IE' => __('Ireland'),
+            'IL' => __('Israel'),
+            'IT' => __('Italy'),
+            'LV' => __('Latvia'),
+            'LI' => __('Liechtenstein'),
+            'LT' => __('Lithuania'),
+            'LU' => __('Luxembourg'),
+            'MT' => __('Malta'),
+            'MC' => __('Monaco'),
+            'ME' => __('Montenegro'),
+            'NL' => __('Netherlands'),
+            'NZ' => __('New Zealand'),
+            'NO' => __('Norway'),
+            'PL' => __('Poland'),
+            'PT' => __('Portugal'),
+            'RO' => __('Romania'),
+            'SM' => __('San Marino'),
+            'RS' => __('Serbia'),
+            'SK' => __('Slovakia'),
+            'SI' => __('Slovenia'),
+            'ES' => __('Spain'),
+            'SE' => __('Sweden'),
+            'CH' => __('Switzerland'),
+            'GB' => __('United Kingdom'),
+            'US' => __('United States'),
+        ];
+    }
+}
