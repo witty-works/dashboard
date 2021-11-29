50f62a27883b2d76741904f4936fb54618298a87
Lukas Kahwe Smith <lukas.smith@witty.works>
2021-11-29 14:21:22 +0100

    omly one team

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -13,19 +13,25 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     @auth
-                    <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('content.dashboard') }}
-                    </x-jet-nav-link>
-                    <x-jet-nav-link href="{{ route('spark.portal') }}" :active="request()->routeIs('spark.portal')">
-                        {{ __('Subscribe') }}
-                    </x-jet-nav-link>
+                        <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                            {{ __('content.dashboard') }}
+                        </x-jet-nav-link>
+                        @if (Auth::user()->currentTeam)
+                            <x-jet-nav-link href="{{ route('spark.portal') }}" :active="request()->routeIs('spark.portal')">
+                                {{ __('Subscribe') }}
+                            </x-jet-nav-link>
+                        @else
+                            <x-jet-nav-link href="{{ route('teams.create') }}">
+                                {{ __('content.create_new_team') }}
+                            </x-jet-nav-link>
+                        @endif
                     @else
-                    <x-jet-nav-link href="{{ route('login') }}" :active="request()->routeIs('login')">
-                        {{ __('content.log_in') }}
-                    </x-jet-nav-link>
-                    <x-jet-nav-link href="{{ route('register') }}" :active="request()->routeIs('register')">
-                        {{ __('content.register') }}
-                    </x-jet-nav-link>
+                        <x-jet-nav-link href="{{ route('login') }}" :active="request()->routeIs('login')">
+                            {{ __('content.log_in') }}
+                        </x-jet-nav-link>
+                        <x-jet-nav-link href="{{ route('register') }}" :active="request()->routeIs('register')">
+                            {{ __('content.register') }}
+                        </x-jet-nav-link>
                     @endauth
                 </div>
             </div>
@@ -33,18 +39,13 @@
             @auth
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 <!-- Teams Dropdown -->
-
-                @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())
+                @if (Laravel\Jetstream\Jetstream::hasTeamFeatures() && Auth::user()->currentTeam)
                     <div class="ml-3 relative">
                         <x-jet-dropdown align="right" width="60">
                             <x-slot name="trigger">
                                 <span class="inline-flex rounded-md">
                                     <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition">
-                                        @if (Auth::user()->currentTeam)
                                         {{ Auth::user()->currentTeam->name }}
-                                        @else
-                                        {{ __('content.create_team')}}
-                                        @endif
 
                                         <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                                             <path fill-rule="evenodd" d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
@@ -144,7 +145,7 @@
                     </x-jet-dropdown>
                 </div>
             </div>
-            @endauth
+            @endif
 
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 @include('language_switcher')
@@ -221,7 +222,8 @@
                 </form>
 
                 <!-- Team Management -->
-                @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())
+                @if (Laravel\Jetstream\Jetstream::hasTeamFeatures() && Auth::user()->currentTeam)
+                )
                     <div class="border-t border-gray-200"></div>
 
                     <div class="block px-4 py-2 text-xs text-gray-400">
@@ -246,7 +248,6 @@
                     <!-- Team Switcher -->
                     <div class="block px-4 py-2 text-xs text-gray-400">
                         {{ __('content.switch_teams') }}
-
                     </div>
 
                     @foreach (Auth::user()->allTeams() as $team)
