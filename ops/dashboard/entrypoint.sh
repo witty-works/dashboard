ff840923e2c78a4f48be9d57cd20fe8fff514264
Sebastian Hutter <seh@foryouandyourcustomers.com>
2021-12-01 16:35:35 +0100

    Make sure storage folder is setup properly

--- ops/dashboard/entrypoint.sh
+++ ops/dashboard/entrypoint.sh
@@ -4,6 +4,18 @@ set -e
 # start sshd for app service access
 /usr/sbin/sshd
 
+# ensure storage folder is setup
+[ ! -d "/var/www/html/storage/framework/sessions" ] \
+    && mkdir -p "/var/www/html/storage/framework/sessions" \
+    && chown www-data:www-data "/var/www/html/storage/framework/sessions"
+[ ! -d "/var/www/html/storage/framework/views" ] \
+    && mkdir -p "/var/www/html/storage/framework/views" \
+    && chown www-data:www-data "/var/www/html/storage/framework/views"
+[ ! -d "/var/www/html/storage/framework/cache" ] \
+    && mkdir -p "/var/www/html/storage/framework/cache" \
+    && chown www-data:www-data "/var/www/html/storage/framework/cache"
+
+# run artisan setup
 php artisan clear-compiled
 php artisan config:cache
 php artisan route:trans:cache
