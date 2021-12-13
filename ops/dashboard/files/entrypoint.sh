fd4d8b5e5987e0bda80d79365d652f189d15bc21
Lukas Kahwe Smith <lukas.smith@witty.works>
2021-12-13 14:44:35 +0100

    run storage link and create the photos dir

--- ops/dashboard/files/entrypoint.sh
+++ ops/dashboard/files/entrypoint.sh
@@ -14,16 +14,21 @@ set -e
 [ ! -d "/var/www/html/storage/framework/cache" ] \
     && mkdir -p "/var/www/html/storage/framework/cache" \
     && chown www-data:www-data "/var/www/html/storage/framework/cache"
+[ ! -d "/var/www/html/storage/app/public/profile-photos" ] \
+    && mkdir -p "/var/www/html/storage/app/public/profile-photos" \
+    && chown www-data:www-data "/var/www/html/storage/app/public/profile-photos"
 
 # # if env vars for basic auth are set lets create the htaccess file
 # # this could be done more elegantely inside app service with azure ad integration
 # # but basic auth is clearer easier for scripted usage etc
 
 # run artisan setup
+cd /var/www/html
 php artisan clear-compiled
 php artisan config:cache
 php artisan route:trans:cache
 php artisan view:cache
 php artisan migrate --force
+php artisan storage:link
 
 /usr/local/bin/apache2-foreground
