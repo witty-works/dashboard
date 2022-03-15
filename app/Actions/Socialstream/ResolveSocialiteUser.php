dd940e717c2c00f655211d90de0b0e22dab510e6
Lukas Kahwe Smith <lukas.smith@witty.works>
2022-03-15 16:32:28 +0100

    ws fix and edge case tweak

--- app/Actions/Socialstream/ResolveSocialiteUser.php
+++ app/Actions/Socialstream/ResolveSocialiteUser.php
@@ -20,16 +20,16 @@ class ResolveSocialiteUser implements ResolvesSocialiteUsers
 
         if ($provider === 'azureadb2c') {
             $user->nickname = $user->user['nickname'] = $user->user['name'] ?: '';
-            $user->name = $user->user['name'] = trim($user->user['given_name'].' '.$user->user['family_name']);
+            $user->name = $user->user['name'] = trim($user->user['given_name'] . ' ' . $user->user['family_name']);
             if (empty($user->user['name'])) {
                 $user->name = $user->user['name'] = $user->user['nickname'];
             }
 
-            $user->email = $user->user['email'] = $user->user['emails'][0] ?: null;
+            $user->email = $user->user['email'] = $user->user['emails'][0] ?: ($user->user['email'] ?: null);
         }
 
         if (Socialstream::generatesMissingEmails()) {
-            $user->email = $user->getEmail() ?? "{$user->id}@{$provider}".config('app.domain');
+            $user->email = $user->getEmail() ?? "{$user->id}@{$provider}" . config('app.domain');
         }
 
         return $user;
