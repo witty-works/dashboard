23b13b6bee913a557ca7964c883f0c60fd068f35
hellasol <hi@solveig.io>
2022-04-13 13:51:56 +0200

    feat(dashboard): complete dashboard styling

--- resources/views/partials/onboarding.blade.php
+++ resources/views/partials/onboarding.blade.php
@@ -61,25 +61,23 @@
 
         <div class="onboarding-steps">
             @foreach($onboardingSteps as $step)
-            @if ($step['link']) <a href="{{ $step['link'] }}"> @endif
-            <div class="onboarding-step-container--{{ $step['state'] }}">
-                <div class="onboarding-step-text-wrapper">
-                    <div class="onboarding-step-title--{{ $step['state'] }}">{{ __($step['title']) }}</div>
-                    <div class="onboarding-step-tagline--{{ $step['state'] }}">{{ __($step['tagline']) }}</div>
-                </div>
-                <div class="onboarding-step-icon-wrapper">
-                    @if ($step['state'] == 'complete')
-                    <img src="{{ url('svg/check-mark.svg') }}" alt="checkmark"/>
-                    @else 
-                    <img src="{{ url('svg/arrow-right.svg') }}" alt="arrow"/>
-                    @endif
-                </div>
+            <div class="onboarding-step-container onboarding-step-{{ $step['state'] }}">
+                @if ($step['link']) <a href="{{ $step['link'] }}"> @endif
+                    <div class="onboarding-step-text-wrapper">
+                        <div class="onboarding-step-title--{{ $step['state'] }}">{{ __($step['title']) }}</div>
+                        <div class="onboarding-step-tagline--{{ $step['state'] }}">{{ __($step['tagline']) }}</div>
+                    </div>
+                @if ($step['link']) </a> @endif
+                @if ($step['state'] == 'complete')
+                <img class="onboarding-step-icon" src="{{ url('svg/check-mark.svg') }}" alt="checkmark" />
+                @else 
+                <img class="onboarding-step-icon" src="{{ url('svg/arrow-right.svg') }}" alt="arrow"/>
+                @endif
             </div>
-            @if ($step['link']) </a> @endif
             @endforeach
         </div>
     @endif
-
+    
     @if($user->invitations->count())
     <div class="mt-6">
         {{ trans_choice('content.open_invitiations', $user->invitations->count()) }}
@@ -111,6 +109,35 @@
         @endif
     </div>
     @endif
-
     @endauth
 </div>
+
+<div class="onboarding-container--two-col">
+    <div class="onboarding-container-col-one">
+        <div class="onboarding-title">{{ __('content.onboarding_quickLinks') }}</div>
+        <div class="onboarding-quick-links-container">
+            <a class="onboarding-iconWrapper" href="{{ route('teams.show', $currentTeam->id) }}">
+                <img src="{{ url('svg/team-setup.svg') }}" alt="team setup"/>
+                <div class="onboarding-icon-description">{{ __('content.onboarding_team_setup') }}</div>
+            </a>
+            <a class="onboarding-iconWrapper" href="{{ route('organization-guidelines', $currentTeam->id) }}">
+                <img src="{{ url('svg/language-guidelines.svg') }}" alt="language guidelines"/>
+                <div class="onboarding-icon-description">{{ __('content.onboarding_language_guidelines') }}</div>
+            </a>
+            <a class="onboarding-iconWrapper" href="{{ route('stripe.portal') }}">
+                <img src="{{ url('svg/payment-billing.svg') }}" alt="payment"/>
+                <div class="onboarding-icon-description">{{ __('content.onboarding_payment') }}</div>
+            </a>
+
+            <a class="onboarding-iconWrapper" href="https://www.witty.works/help">
+                <img src="{{ url('svg/support.svg') }}" alt="support"/>
+                <div class="onboarding-icon-description">{{ __('content.onboarding_support') }}</div>
+            </a>
+        </div>
+    </div>
+    <div class="onboarding-container-col-two">
+        <div class="onboarding-title">{{ __('content.onboarding_team_stats') }}</div>
+        <img class="onboarding-analytics-img" src="{{ url('svg/analytics-coming-soon.svg') }}" alt="analytics"/>
+    </div>
+</div>
+
