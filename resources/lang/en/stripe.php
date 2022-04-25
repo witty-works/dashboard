f4174601dcb8d07dfc97d4e0cb4fc6d985ab1999
Lukas Kahwe Smith <lukas.smith@witty.works>
2022-04-25 09:55:48 +0200

    remove unused billing/pricing page code

--- resources/lang/en/stripe.php
+++ resources/lang/en/stripe.php
@@ -1,15 +1,9 @@
 <?php
 
 return [
-    'checkout' => 'Subscribe now',
     'witty_me' => 'Witty Me',
     'witty_teams' => 'Witty Teams',
     'witty_enterprise' => 'Witty Enterprise',
-    'schedule_demo' => 'Schedule a demo',
-    'current_plan' => 'Your current plan',
     'billing' => 'Go to billing portal',
-    'pricing' => 'Pricing',
-    'register_now' => 'Register now',
-    'price' => '/ user / month, billed annually',
     'contact_sales' => 'Contact Sales',
 ];
