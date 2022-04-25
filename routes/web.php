bf72eccca537a955a318ddc9b407b2e5f92465a2
Lukas Kahwe Smith <lukas.smith@witty.works>
2022-04-25 09:56:04 +0200

    removed unused code

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\Livewire\GuidelinesController;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\SubscribeRedirectController;
 use App\Http\Controllers\StripeController;
 use App\Http\Controllers\WebhookController;
 
