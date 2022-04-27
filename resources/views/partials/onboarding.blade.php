ae4179e7b00b709f0032d2a8250da74111009c97
hellasol <hi@solveig.io>
2022-04-27 13:52:30 +0200

    fix: noopener

--- resources/views/partials/onboarding.blade.php
+++ resources/views/partials/onboarding.blade.php
@@ -88,7 +88,7 @@ if(!$currentTeam || $user->can('update', $currentTeam)) {
             </div>
         </div>
         <div class='wittyworks-upgrade-banner-button-container'>
-            <a class='wittyworks-upgrade-banner-button' href='https://www.witty.works/select-browser' target='_blank'>
+            <a class='wittyworks-upgrade-banner-button' href='https://www.witty.works/select-browser' target='_blank' rel="noopener">
                 {{ __('content.onboarding_install_witty_button') }}
             </a>
         </div>
