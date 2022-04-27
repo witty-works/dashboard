066d773ea374d86b641aee5da0a91f79bf460057
hellasol <hi@solveig.io>
2022-04-27 13:48:34 +0200

    fix: styling and removed double js

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,18 +3,6 @@ require('./bootstrap');
 import * as Sentry from "@sentry/browser";
 import { Integrations } from "@sentry/tracing";
 
-window.addEventListener('load', () => {
-    const wittyCode = document.querySelector('witty-code');
-    if (wittyCode) {
-        const extensionId = wittyCode.getAttribute('extension-id');
-        const usersOptionPage = `chrome-extension://${extensionId}/options.html`;
-        console.log('witty is installed', usersOptionPage);
-    } 
-    else {
-        console.log('Witty not installed');
-    }
-});
-
 if (window.sentry_dsn) {
     Sentry.init({
         dsn: window.sentry_dsn,
