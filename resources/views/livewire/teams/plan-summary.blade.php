fa369befefa5160bc45e9d5129d4b7685ab5a2c0
Lukas Kahwe Smith <lukas.smith@witty.works>
2022-05-12 21:16:33 +0200

    simplified text for when license updates will be done

--- resources/views/livewire/teams/plan-summary.blade.php
+++ resources/views/livewire/teams/plan-summary.blade.php
@@ -41,11 +41,11 @@
                     </div>
                 @elseif ($team->getTotalUserCount() != $team->getUserLicensesCount())
                     <div>
-                        @if($team->subscribed() && $team->subscription()->update_user_licenses_at)
+                        @if($team->subscribed())
                         @if($team->getTotalUserCount() > $team->getUserLicensesCount())
-                        {{ trans_choice('teams.user_licenses_count_will_be_increased_updated_at', $team->getTotalUserCount() - $team->getUserLicensesCount(), ['diff' => $team->getTotalUserCount() - $team->getUserLicensesCount(), 'in' => $team->subscription()->update_user_licenses_at->diffForHumans()]) }}
+                        {{ trans_choice('teams.user_licenses_count_will_be_increased_updated_at', $team->getTotalUserCount() - $team->getUserLicensesCount(), ['diff' => $team->getTotalUserCount() - $team->getUserLicensesCount()]) }}
                         @else
-                        {{ trans_choice('teams.user_licenses_count_will_be_decreased_updated_at', $team->getUserLicensesCount() - $team->getTotalUserCount(), ['diff' => $team->getUserLicensesCount() - $team->getTotalUserCount(), 'in' => $team->subscription()->update_user_licenses_at->diffForHumans()]) }}
+                        {{ trans_choice('teams.user_licenses_count_will_be_decreased_updated_at', $team->getUserLicensesCount() - $team->getTotalUserCount(), ['diff' => $team->getUserLicensesCount() - $team->getTotalUserCount()]) }}
                         @endif
                         @elseif(Auth::user()->ownsTeam($team) && $team->getTotalUserCount() > $team->getUserLicensesCount())
                         {!! trans_choice('teams.please_remove_users_or_upgrade', $team->getTotalUserCount() - $team->getUserLicensesCount(), ['diff' => $team->getTotalUserCount() - $team->getUserLicensesCount(), 'url' => route('stripe.portal')]) !!}
