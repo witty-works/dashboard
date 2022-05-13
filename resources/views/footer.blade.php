5816d4a4c5bdd38685bd16767c27fc376ef3946a
Lukas Kahwe Smith <lukas.smith@witty.works>
2022-05-13 11:25:02 +0200

    add roadmap to footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -5,20 +5,23 @@
             <div class="flex">
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex sm:items-center">
-                    <x-jet-nav-link href="https://www.witty.works/terms">
-                        {{ __('content.terms') }}
-                    </x-jet-nav-link>
                     <x-jet-nav-link href="https://www.witty.works/contact-sales">
                         {{ __('content.contact') }}
                     </x-jet-nav-link>
                     <x-jet-nav-link href="https://www.witty.works/demo">
                         {{ __('content.book-demo') }}
                     </x-jet-nav-link>
-                    <x-jet-nav-link href="https://www.witty.works/privacy">
-                        {{ __('content.privacy') }}
+                    <x-jet-nav-link href="https://roadmap.witty.works">
+                        Roadmap
                     </x-jet-nav-link>
                     <x-jet-nav-link href="https://www.witty.works/trust-and-security">
                         {{ __('content.trust-and-security') }}
+                    </x-jet-nav-link>
+                    <x-jet-nav-link href="https://www.witty.works/terms">
+                        {{ __('content.terms') }}
+                    </x-jet-nav-link>
+                    <x-jet-nav-link href="https://www.witty.works/privacy">
+                        {{ __('content.privacy') }}
                     </x-jet-nav-link>
                     <x-jet-nav-link href="https://www.witty.works/impressum">
                         {{ __('content.imprint') }}
