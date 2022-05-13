d79874e3ee0b7f69d5c492b47e86b5909c4a82fd
Lukas Kahwe Smith <lukas.smith@witty.works>
2022-05-13 09:58:33 +0200

    fix email config defaults

--- config/mail.php
+++ config/mail.php
@@ -92,8 +92,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'support@witty.works'),
+        'name' => env('MAIL_FROM_NAME', 'Witty - Dashboard'),
     ],
 
     /*
