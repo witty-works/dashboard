f1d765ed92784addcd1aa50f1972ba802234bbee
Lukas Kahwe Smith <lukas.smith@witty.works>
2022-05-16 17:27:48 +0200

    align normal footer and hamburger footer

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -27,7 +27,7 @@
             <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
                 {{ $slot }}
 
-                @include('footer')
+                @include('partials.footer')
             </div>
     </body>
 </html>
