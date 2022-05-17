c6736cbf5b439cd87319a5fbd2c040d64f6002f0
Lukas Kahwe Smith <lukas.smith@witty.works>
2022-05-17 16:11:49 +0200

    prevent redirecting non team owners back to pricing page

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Response;
 
 class StripeController extends Controller
 {
@@ -18,41 +20,58 @@ class StripeController extends Controller
             return redirect(route('teams.create'));
         }
 
-        return $this->portal($request);
+        $response = $this->goToPortal($user);
+        if ($response instanceof Response) {
+            return $response;
+        }
+
+        return redirect(route('dashboard'))->banner(
+            __('teams.ask_owner_to_buy_or_leave_to_create_own_team', ['name' => $team->owner->name, 'email' => $team->owner->email]),
+        );
     }
 
     public function portal(Request $request)
     {
         $user = $request->user();
-        $team = $user->currentTeam;
-        if ($team && $user->ownsTeam($team)) {
-            $subscription = $team->subscription();
-            if ($subscription) {
-                if ($subscription->isPaidByInvoice()) {
-                    return redirect('mailto:sales@witty.works');
-                }
-
-                return $team->redirectToBillingPortal(
-                    route('dashboard'),
-                    ['locale' => app()->getLocale()]
-                );
-            }
-
-            return $team
-                ->allowPromotionCodes()
-                ->checkout(
-                    [[
-                        'price' => config('stripe.plans.witty_teams.price_id'),
-                        'quantity' => $team->getTotalUserCount()
-                    ]],
-                    [
-                        'success_url' => route('dashboard'),
-                        'cancel_url' => route('teams.show', ['team' => $team]),
-                        'mode' => 'subscription'
-                    ]
-                )->redirect();
+        $response = $this->goToPortal($user);
+        if ($response instanceof Response) {
+            return $response;
         }
 
         return redirect('https://www.witty.works/pricing');
+    }
+
+    protected function goToPortal(User $user)
+    {
+        $team = $user->currentTeam;
+        if (!$team || !$user->ownsTeam($team)) {
+            return false;
+        }
+
+        $subscription = $team->subscription();
+        if ($subscription) {
+            if ($subscription->isPaidByInvoice()) {
+                return redirect('mailto:sales@witty.works');
+            }
+
+            return $team->redirectToBillingPortal(
+                route('dashboard'),
+                ['locale' => app()->getLocale()]
+            );
+        }
+
+        return $team
+            ->allowPromotionCodes()
+            ->checkout(
+                [[
+                    'price' => config('stripe.plans.witty_teams.price_id'),
+                    'quantity' => $team->getTotalUserCount()
+                ]],
+                [
+                    'success_url' => route('dashboard'),
+                    'cancel_url' => route('teams.show', ['team' => $team]),
+                    'mode' => 'subscription'
+                ]
+            )->redirect();
     }
 }
