bce3e6060cda6e2bc61e6d6e99b929a23af8a6fc
Lukas Kahwe Smith <lukas.smith@witty.works>
2022-06-03 08:47:39 +0200

    updated translations

--- resources/lang/de/teams.php
+++ resources/lang/de/teams.php
@@ -21,7 +21,7 @@ return [
     'pricing' => 'Preise',
     'user_licenses_count_will_be_increased_updated_at' => '{1} Ein:e Lizenz wird in ein paar Minuten hinzugefügt | :diff Lizenzen werden in ein paar Minuten hinzugefügt',
     'user_licenses_count_will_be_decreased_updated_at' => '{1} Ein:e Lizenz wird in ein paar Munuten entfernt | :diff Lizenzen werden in ein paar Minuten entfernt',
-    'please_remove_users_or_upgrade' => '{1} Bitte entferne eine Person oder wechsele deinen <a href=":url">Plan</a>, um alle Teammitglieder:innen zu reaktivieren. | Bitte entferne :diff Personen oder wechsele deinen <a href=":url">Abonnement</a>, um alle Teammitglieder:innen wieder zu aktivieren.',
+    'please_remove_users_or_upgrade' => '{1} Bitte entferne eine Person oder wechsele deinen <a href=":url">Abonnement</a>, um alle Teammitglieder:innen zu reaktivieren. | Bitte entferne :diff Personen oder wechsele deinen <a href=":url">Abonnement</a>, um alle Teammitglieder:innen wieder zu aktivieren.',
     'end_date' => 'Enddatum',
     'team_owner' => 'Team-Eigentümer:in',
     'upgrade_to_witty_teams' => 'Bitte wechsle auf das Abonnement <a href="https://www.witty.works/pricing" target="_blank">Witty Teams</a>',
