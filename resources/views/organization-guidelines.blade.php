ede5b2b385e53858b95ac3b668afd0daa1fbc380
Lukas Kahwe Smith <lukas.smith@witty.works>
2022-06-09 17:38:43 +0200

    improved alignment and text on the team language pages

--- resources/views/organization-guidelines.blade.php
+++ resources/views/organization-guidelines.blade.php
@@ -1,33 +1,33 @@
 <div>
-    <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto py-10">
         @livewire('organization-guidelines.language', ['team' => $team])
     </div>
 
-    <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto py-10">
         @livewire('organization-guidelines.english', ['team' => $team])
     </div>
 
-    <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto py-10">
         @livewire('organization-guidelines.german', ['team' => $team])
     </div>
 
-    <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto py-10">
         @livewire('organization-guidelines.expert-mode', ['team' => $team])
     </div>
 
-    <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto py-10">
         @livewire('organization-guidelines.inspirations', ['team' => $team])
     </div>
 
-    <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto py-10">
         @livewire('organization-guidelines.inclusive', ['team' => $team])
     </div>
 
-    <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto py-10">
         @livewire('organization-guidelines.style', ['team' => $team])
     </div>
 
-    <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto py-10">
         @livewire('organization-guidelines.orthography', ['team' => $team])
     </div>
 </div>
--- resources/views/term-replacement.blade.php
+++ resources/views/term-replacement.blade.php
@@ -1,13 +1,13 @@
 @if (Auth::user()->hasTeamPermission($team, 'edit_guidelines'))
 <div>
-    <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto py-10">
         @livewire('term-replacement.form', ['team' => $team])
     </div>
 </div>
 @endif
 
 <div>
-    <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto py-10">
         @livewire('term-replacement.show', ['team' => $team])
     </div>
 </div>
