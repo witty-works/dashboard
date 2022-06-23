4f3c7e477980d15ae2e16c8cbe6e36d3301987bb
Lukas Kahwe Smith <lukas.smith@witty.works>
2022-06-23 09:49:05 +0200

    use rel noopener

--- resources/views/partials/witty-teams-only.blade.php
+++ resources/views/partials/witty-teams-only.blade.php
@@ -4,6 +4,7 @@
 <a class="witty-teams-only"
     href="{{ $url }}"
     target="_blank"
+    rel="noopener"
     {{ __('teams.upgrade_to_witty_teams') }}" title="{{ __('teams.upgrade_to_witty_teams') }}"
 >
     {{ __('teams.witty_teams_only') }}
