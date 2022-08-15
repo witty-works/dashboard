deafebd05403dfdbd0aa6311a646f774f1600ae2
Lukas Kahwe Smith <lukas.smith@witty.works>
2022-08-15 17:31:18 +0200

    disable team deleting

--- resources/views/teams/show.blade.php
+++ resources/views/teams/show.blade.php
@@ -15,14 +15,6 @@
 
             @livewire('teams.team-member-manager', ['team' => $team])
 
-            @if (Gate::check('delete', $team) && ! $team->personal_team)
-                <x-jet-section-border />
-
-                <div class="mt-10 sm:mt-0">
-                    @livewire('teams.delete-team-form', ['team' => $team])
-                </div>
-            @endif
-
             @livewire('teams.store-context', ['team' => $team])
         </div>
     </div>
