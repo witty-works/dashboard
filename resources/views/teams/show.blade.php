139f8a85722201beb295ba0415ff987fbd6d457a
Lukas Kahwe Smith <lukas.smith@witty.works>
2022-08-29 11:30:43 +0200

    move team name edit to subscription page

--- resources/views/teams/show.blade.php
+++ resources/views/teams/show.blade.php
@@ -11,8 +11,6 @@
             <div>
                 <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
                     @livewire('teams.team-member-manager', ['team' => $team])
-
-                    @livewire('teams.update-team-name-form', ['team' => $team])
                 </div>
             </div>
         </div>
