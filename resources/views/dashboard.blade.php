f0255fb955239ce0aa18a05fd883d433298bc2b8
hellasol <hi@solveig.io>
2022-09-05 13:20:45 +0300

    fix: dashboard on small screens

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <div class="wittyworks-navigation-wrapper">@livewire('navigation-menu')</div>
     <div class="wittyworks-page-wrapper">
-        <div class="wittyworks-page">
+        <div class="wittyworks-page lg:ml-20">
             @include('partials.extension-check')
             @include('partials.invite-check')
             @include('partials.invitations')
