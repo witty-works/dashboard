2af7693bcea8c86127c6a784504c4d261fb1d9cb
Lukas Kahwe Smith <lukas.smith@witty.works>
2022-09-20 10:52:14 +0200

    remove laravel logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,11 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
-@else
 <img src="{{ url('witty-logo-white.png') }}" alt="Witty Works" />
-@endif
 </a>
 </td>
 </tr>
