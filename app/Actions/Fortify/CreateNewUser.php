025e9a481f4901c0c39f841851b484afb83348dd
Lukas Kahwe Smith <lukas.smith@witty.works>
2022-09-27 09:41:52 +0200

    ensure all users have a current team after creation

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -2,7 +2,7 @@
 
 namespace App\Actions\Fortify;
 
-use App\Models\Team;
+use App\Http\Middleware\SwitchToTeam;
 use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -29,23 +29,8 @@ class CreateNewUser implements CreatesNewUsers
                 'email' => $input['email'],
                 'password' => Hash::make($input['password']),
             ]), function (User $user) {
-                $this->createTeam($user);
+                SwitchToTeam::ensureTeam($user);
             });
         });
-    }
-
-    /**
-     * Create a personal team for the user.
-     *
-     * @param  \App\Models\User  $user
-     * @return void
-     */
-    protected function createTeam(User $user)
-    {
-        $user->ownedTeams()->save(Team::forceCreate([
-            'user_id' => $user->id,
-            'name' => explode(' ', $user->name, 2)[0] . "'s Team",
-            'personal_team' => true,
-        ]));
     }
 }
--- app/Actions/Socialstream/CreateUserFromProvider.php
+++ app/Actions/Socialstream/CreateUserFromProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Actions\Socialstream;
 
-use App\Models\Team;
+use App\Http\Middleware\SwitchToTeam;
 use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use JoelButcher\Socialstream\Contracts\CreatesConnectedAccounts;
@@ -56,6 +56,8 @@ class CreateUserFromProvider implements CreatesUserFromProvider
                 $user->switchConnectedAccount(
                     $this->createsConnectedAccounts->create($user, $provider, $providerUser)
                 );
+
+                SwitchToTeam::ensureTeam($user);
             });
         });
     }
