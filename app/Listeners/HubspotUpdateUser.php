8b6f1de3bf5640d0c36a65dd092b8550718ff674
Lukas Kahwe Smith <lukas.smith@witty.works>
2022-10-02 11:03:06 +0200

    fix undefined property in Hubspot sync ##279

--- app/Listeners/HubspotUpdateUser.php
+++ app/Listeners/HubspotUpdateUser.php
@@ -2,16 +2,21 @@
 
 namespace App\Listeners;
 
+use App\Events\AbstractSubscription;
+use App\Events\UserEvent;
 use App\Jobs\SyncUserToHubSpot;
+use Laravel\Jetstream\Events\TeamEvent;
 
 class HubspotUpdateUser
 {
     public function handle($event)
     {
-        if ($event->user) {
+        if ($event instanceof UserEvent) {
             $user = $event->user;
-        } elseif ($event->team) {
+        } elseif ($event instanceof TeamEvent || $event instanceof AbstractSubscription) {
             $user = $event->team->owner;
+        } else {
+            return;
         }
 
         dispatch(new SyncUserToHubSpot($user));
