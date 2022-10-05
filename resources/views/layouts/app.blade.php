f06c64cacde8333b2d39ef06a6859666652df0b4
Lukas Kahwe Smith <lukas.smith@witty.works>
2022-10-05 16:20:03 +0200

    download google fonts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,9 @@
         <title>{{ config('app.name') }}</title>
 
         <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        @googlefonts('lato')
+        @googlefonts('ibarra')
+        @googlefonts('roboto')
 
         <!-- Styles -->
         <link rel="icon" type="image/webp" href="{{ URL::asset('/witty-icon-color-inverted@2x-1.webp') }}"/>
