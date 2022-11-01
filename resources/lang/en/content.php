2867e42534daa350a97bd1bec821309843e0003f
Lukas Kahwe Smith <lukas.smith@witty.works>
2022-11-01 21:14:25 +0100

    add subscribe banner

--- resources/lang/en/content.php
+++ resources/lang/en/content.php
@@ -285,4 +285,7 @@ Your Witty team',
 
     'mailing_consent_reject' => 'I do not want to stay uptodate in Witty news',
     'upgrade_title' => 'upgrade title',
+    'upgrade_text' => 'upgrade text',
+    'upgrade_ask_owner_text' => 'upgrade ask owner text',
+    'upgrade_button' => 'upgrade button',
 ];
