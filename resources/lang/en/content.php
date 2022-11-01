86d7a5c0de11f422d4bb965207d9afe6fcee03e6
Lukas Kahwe Smith <lukas.smith@witty.works>
2022-11-01 21:13:54 +0100

    make it possible to reject the mailing subscription banner

--- resources/lang/en/content.php
+++ resources/lang/en/content.php
@@ -210,4 +210,6 @@ Your Witty team',
     'onboarding_login_witty_title' => 'Please log into Witty',
     'onboarding_login_witty_text' => 'In order to use Witty, please sign into the Witty browser extension.',
     'onboarding_login_witty_button' => 'Sign into Witty',
+    'mailing_consent_reject' => 'I do not want to stay uptodate in Witty news',
+    'upgrade_title' => 'upgrade title',
 ];
