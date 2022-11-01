d34d771e9ca6f945f3f6b431343250dc921fc366
Lukas Kahwe Smith <lukas.smith@witty.works>
2022-11-01 21:15:50 +0100

    subscriptions should be done from the subscription page

--- resources/views/livewire/organization-term-replacement/form.blade.php
+++ resources/views/livewire/organization-term-replacement/form.blade.php
@@ -5,7 +5,7 @@
 
     <x-slot name="description">
         @if($team->getFalsePositivesLimitReached() && Auth::user()->ownsTeam($team) && !$team->subscribed())
-           {!! __('guidelines.term_replacement_limit_reached', ['max_count' => $team->getTermReplacementsCount(), 'url' => route('stripe.portal')]) !!}
+           {!! __('guidelines.term_replacement_limit_reached', ['max_count' => $team->getTermReplacementsCount(), 'url' => route('teams.subscription')]) !!}
         @else
             {!! Str::markdown(__('guidelines.create_new_term_replacement_description')) !!}
         @endif
