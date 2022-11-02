02f6e23205830d4efcba6cc9e591e5f34703e28e
Lukas Kahwe Smith <lukas.smith@witty.works>
2022-11-02 08:44:13 +0100

    align layout between my profile and team subscription page

--- resources/lang/de/teams.php
+++ resources/lang/de/teams.php
@@ -33,7 +33,6 @@ return [
     'total_of_max_used_dictionary' => ':total von :max_count möglichen Einträgen in das Team-Wörterbuch wurden gemacht',
     'total_of_max_used_ignored' => ':total von :max_count möglichen Einträgen in die Liste ignorierter Wörter wurden gemacht ',
     'locked_by_team' => 'Diese Funktion wird von deiner*m Team-Admin gesetzt.',
-    'team_plan_headline' => 'Team-Abonnement',
     'license_count_error' => 'Beim Aktualisieren der Lizenzanzahl ist ein Fehler aufgetreten.',
     'license_count_too_small_error' => 'Bitte erhöhe die Anzahl der Lizenzen oder entferne Personen aus dem Team.',
     'license_count_label' => 'Lizenzverwaltung',
--- resources/lang/en/teams.php
+++ resources/lang/en/teams.php
@@ -10,7 +10,6 @@ return [
     'plan_summary' => 'Your Subscription',
     'plan_name' => 'Current plan',
     'plan_headline' => 'Subscription',
-    'team_plan_headline' => 'Team Subscription',
     'profile' => 'Profile',
     'upgrade' => 'Upgrade to Witty Teams',
     'pricing' => 'Pricing',
