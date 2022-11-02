001d6a4671eb643bb4c2b023213f90f90707743e
Lukas Kahwe Smith <lukas.smith@witty.works>
2022-11-02 08:44:32 +0100

    hide subscription banner on the subscription page

--- resources/views/partials/banners.blade.php
+++ resources/views/partials/banners.blade.php
@@ -15,6 +15,6 @@ $showMailing = $user->has_consented_to_mailing === null;
 @include('partials.invite-check')
 @elseif($showMailing)
 @include('partials.mailing-consent', ['user' => $user])
-@elseif(!$team->subscribed())
+@elseif(!$team->subscribed() && !Route::is('teams.subscription'))
 @include('partials.subscribe-banner', ['user' => $user])
 @endif
