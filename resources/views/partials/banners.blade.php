ddae4991273c6b8711bae93e0ca9c0003ce53894
Barbara Schülé <barbara.schule@witty.works>
2022-11-03 15:15:20 +0100

    adapt UX and wording of mailing banner

--- resources/views/partials/banners.blade.php
+++ resources/views/partials/banners.blade.php
@@ -4,7 +4,7 @@ $team = $user->currentTeam;
 
 $showInviteCheck = empty($hideInviteCheck) && $team && $team->getTotalUserWithInvitationsCount() <= 1;
 $showInvitations = $user->invitations->count();
-$showMailing = $user->has_consented_to_mailing === null;
+$showMailing = !$user->has_consented_to_mailing;
 @endphp
 
 @include('partials.extension-check')
