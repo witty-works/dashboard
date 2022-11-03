29dd57c3010d33b5b762e381be99bbcaea4cfbd1
Barbara Schülé <barbara.schule@witty.works>
2022-11-03 16:21:55 +0100

    various css changes

--- resources/views/livewire/organization-guidelines/german.blade.php
+++ resources/views/livewire/organization-guidelines/german.blade.php
@@ -7,10 +7,17 @@
         {!! Str::markdown(__('guidelines.manage_organization_guidelines_description_german')) !!}
     </x-slot>
 
-    <x-slot name="form" submit="updateLanguageGuidelinesGerman">  
-        <div class="margin-bottom">
-            {!! __('guidelines.gendered_roles_format') !!}
-        </div>
+    <x-slot name="form" submit="updateLanguageGuidelinesGerman">
+
+        <div class="lato-paragraph-text-p margin-bottom">{!! __('guidelines.gendered_roles_format') !!}</div>
+
+
+        <div class="lato-small-text-p"> {{ __('guidelines.german_gender_ending') }}</div>
+        <x-select id="german_gender_ending"
+            :options="\App\Models\GuidelinesInterface::GERMAN_GENDER_ENDING"
+            class="guidelines-form-section-dropdown lato-small-text-p margin-bottom"
+            wire:model.defer="german_gender_ending"
+        />
 
 
         <div class="lato-small-text-p"> {{ __('guidelines.german_gender_ending') }}</div>
@@ -39,22 +46,8 @@
             </div>
         </div>
 
-        <div class="margin-bottom">
-            {!! __('guidelines.german_gender_ending') !!}
-        </div>
 
-        <div class="lato-small-text-p">{!! __('guidelines.manage_organization_guidelines_description_german_gender_ending_sub_title') !!}</div>
-        <div class="flex flex-row">
-            <div>
-                <x-select id="german_gender_ending"
-                    :options="\App\Models\GuidelinesInterface::GERMAN_GENDER_ENDING"
-                    class="guidelines-form-section-dropdown"
-                    wire:model.defer="german_gender_ending"
-                />
-
-                <x-jet-input-error for="german_gender_ending" class="mt-2" />
-            </div>
-        </div>
+        <x-jet-input-error for="german_gender_ending" class="mt-2" />
     </x-slot>
 
     <x-slot name="actions">
