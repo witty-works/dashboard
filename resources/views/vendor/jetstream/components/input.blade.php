4af3927be382f5ca2e806ec0d928b4c848ee618b
Lukas Kahwe Smith <lukas.smith@witty.works>
2022-11-11 09:01:54 +0100

    reduce code duplication

--- resources/views/vendor/jetstream/components/input.blade.php
+++ resources/views/vendor/jetstream/components/input.blade.php
@@ -1,7 +1,9 @@
 @props(['disabled' => false])
-
+@php
+    $attributes = $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm border-radius']);
+@endphp
 @if($attributes->get('type') === 'textarea')
-<textarea {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm border-radius']) !!}>{{ $attributes->get('value') }}</textarea>
+<textarea {{ $disabled ? 'disabled' : '' }} {!! $attributes !!}>{{ $attributes->get('value') }}</textarea>
 @else
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm border-radius']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes !!}>
 @endif
