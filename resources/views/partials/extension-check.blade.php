65ec425d2b71e09e377d17d7035c43e0b7b5f26e
hellasol <hi@solveig.io>
2022-11-30 10:00:44 +0100

    WIP: upgrade extension banner

--- resources/views/partials/extension-check.blade.php
+++ resources/views/partials/extension-check.blade.php
@@ -4,15 +4,22 @@
 
         const installWitty = document.querySelector('#install-witty');
         const loginWitty = document.querySelector('#login-witty');
+        const upgradeWittyVersion = document.querySelector('#upgrade-witty-version');
 
         if (!wittyIsInstalled) {
             installWitty.style.display = 'flex';
-                   } else {
+        } else {
             loginUrl = wittyIsInstalled.getAttribute('login-url')
             if (loginUrl) {
                 const loginWittyUrl = document.getElementById('login-witty-url');
                 loginWittyUrl.setAttribute('href', loginUrl + '?target=' + encodeURIComponent(window.location.href))
                 loginWitty.style.display = 'flex';
+            }
+
+            const extensionVersion = wittyIsInstalled.getAttribute('extension-version');
+            const newestVersion = '3.3.3' //TODO: get newest version 
+            if (extensionVersion !== newestVersion) { 
+                upgradeWittyVersion.style.display = 'flex';
             }
         }
     });
@@ -46,6 +53,23 @@
         <div class="wittyworks-upgrade-banner-button-container">
             <a id="login-witty-url" class="button primary-button-purple" href="">
                 {{ __('content.onboarding_login_witty_button') }}
+            </a>
+        </div>
+    </div>
+
+    <div id="upgrade-witty-version" style="display: none" class="wittyworks-upgrade-banner">
+        <div>
+            <div class="wittyworks-upgrade-banner-title">
+                {{ __('content.upgrade_witty_version_title') }}
+            </div>
+            <div class="wittyworks-upgrade-banner-text">
+                {{ __('content.upgrade_witty_version_text') }}
+            </div>
+        </div>
+        <div class="wittyworks-upgrade-banner-button-container">
+        <!-- TODO: Add correct link to button, link to chrome store? -->
+        <a class="button primary-button-purple" href="https://www.witty.works/select-browser" target="_blank" rel="noopener">
+                {{ __('content.upgrade_witty_version_button') }}
             </a>
         </div>
     </div>
