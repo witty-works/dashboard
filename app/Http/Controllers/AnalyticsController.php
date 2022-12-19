witty-works/dashboard

commit ccff2bc4d0f2d22cfeda3c59f6b6b2f85401082c

add kpi storing and use on the analytics page to compute user_count and writing_streak (#380)

app/Http/Controllers/AnalyticsController.php
@@ -2,24 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use App\Categories;
+use App\Helpers\Categories;
+use App\Models\Kpi;
+use App\Models\Team;
 use App\Models\User;
-use Carbon\Carbon;
+use App\Helpers\Posthog;
+use DateTime;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
-use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\DB;
 
 class AnalyticsController extends Controller
 {
-    protected $personalApiKey;
     protected $url;
     protected $refresh;
 
     public function __construct(Request $request)
     {
-        $this->personalApiKey = config('posthog.personal_api_key');
-
         $projectId = $request->get('project_id', config('posthog.project_id'));
         $this->url = config('posthog.host') . "/api/projects/$projectId/insights/trend";
         $this->refresh = $request->get('refresh', false);
@@ -65,22 +64,7 @@ class AnalyticsController extends Controller
             abort(403);
         }
 
-        $postHogId = config('posthog.dashboard_user_id_override');
-        if (empty($postHogId)) {
-            $postHogId = $request->user()->posthogId();
-        }
-
-        $properties = [
-            'type' => 'AND',
-            'values' => [
-                [
-                    'key' => 'dashboard_id',
-                    'value' => $postHogId,
-                    'operator' => 'exact',
-                    'type' => 'person',
-                ]
-            ]
-        ];
+        $properties = PostHog::getUserFilter($request->user());
 
         return $this->fetchJson($request, $properties);
     }
@@ -90,41 +74,9 @@ class AnalyticsController extends Controller
         $user = $request->user();
         $this->teamAnalyticsAllowed($user);
 
-        $postHogId = config('posthog.dashboard_team_id_override');
-        if (empty($postHogId)) {
-            $postHogId = $user->currentTeam->posthogId();
-        }
+        $properties = PostHog::getOrganizationFilter($user->currentTeam);
 
-        $properties = [
-            'type' => 'AND',
-            'values' => [
-                [
-                    'key' => 'response__organizationId',
-                    'value' => $postHogId,
-                    'operator' => 'exact',
-                    'type' => 'event',
-                ]
-            ]
-        ];
-
-        return $this->fetchJson($request, $properties);
-    }
-
-    protected function fetchData($filter)
-    {
-        $key = 'posthog:' . md5($this->url) . ':' . md5(serialize($filter));
-        if ($this->refresh) {
-            Cache::forget($key);
-        }
-
-        return Cache::remember($key, config('posthog.insights_cache_time'), function () use ($filter) {
-            $response = Http::withToken($this->personalApiKey)
-                ->post($this->url, $filter);
-
-            $data = $response->collect()->all();
-            $data['last_refresh'] = Carbon::now();
-            return $data;
-        });
+        return $this->fetchJson($request, $properties, $user->currentTeam);
     }
 
     protected function fetchEventData($events, $properties, $interval, $from, $math = 'total')
@@ -145,7 +97,7 @@ class AnalyticsController extends Controller
         foreach ($events as $event) {
             $filter['events'][0]['id'] = $event;
 
-            $response = $this->fetchData($filter);
+            $response = PostHog::fetchData($filter, $this->url);
             if (isset($response['result'][0])) {
                 $data['events'][$event] = array_combine($response['result'][0]['days'], $response['result'][0]['data']);
             }
@@ -182,7 +134,7 @@ class AnalyticsController extends Controller
         foreach ($events as $event) {
             $filter['events'][0]['id'] = $event;
 
-            $response = $this->fetchData($filter);
+            $response = PostHog::fetchData($filter, $this->url);
             if (isset($response['result'])) {
                 foreach ($response['result'] as $value) {
                     $data['events'][$event][$value['breakdown_value']] = $value['aggregated_value'];
@@ -194,9 +146,18 @@ class AnalyticsController extends Controller
         return $data;
     }
 
-    protected function fetchJson(Request $request, $properties)
+    protected function numWeeks(DateTime $firstDate, DateTime $secondDate)
+    {
+        $differenceInDays = $firstDate->diff($secondDate)->days;
+        $differenceInWeeks = $differenceInDays / 7;
+
+        return floor($differenceInWeeks);
+    }
+
+    protected function fetchJson(Request $request, $properties, $model = null)
     {
         $from = $this->fetchFrom($request);
+        $fromPosthog = "-{$from}d";
         $interval = $this->fetchInterval($request);
         $chart = $request->get('chart');
 
@@ -240,16 +201,98 @@ class AnalyticsController extends Controller
 
         switch ($chart) {
             case 'dau':
-                $events = ['check', 'popover_open', 'alternative', 'ignore', 'learning_bites'];
-                $data = $this->fetchEventData($events, $properties, $interval, $from, 'dau');
-                foreach ($data['events']['check'] as $day => $value) {
-                    $data['events']['user_count'][$day] = $request->user()->currentTeam->getTotalUserCount();
+                $events = ['popover_open', 'alternative', 'ignore', 'learning_bites'];
+                $data = $this->fetchEventData($events, $properties, $interval, $fromPosthog, 'dau');
+
+                if ($model instanceof Team) {
+                    $fromUserCount = $from + 10;
+                    $userCount = Kpi::where('team_id', $model->id)
+                        ->where('kpi', Kpi::TEAM_COUNT)
+                        ->whereRaw("date > DATE_SUB(NOW(), INTERVAL {$fromUserCount} DAY)")
+                        ->select('date', 'value')
+                        ->pluck('value', 'date')
+                        ->toArray();
+
+                    foreach ($data['events']['popover_open'] as $day => $value) {
+                        $value = (int) $value;
+                        // the check here is to handle the case when a team adds and removes users over the course of the week
+                        if (isset($userCount[$day]) && $userCount[$day] > $value) {
+                            $value = $userCount[$day];
+                        }
+                        // handle missing data in the KPI table, this should eventually never happen
+                        if ($value === 0) {
+                            $value = $model->getTotalUserCount();
+                        }
+                        $data['events']['user_count'][$day] = $value;
+                    }
                 }
 
                 break;
             case 'total':
                 $events = ['check', 'popover_open', 'alternative', 'ignore', 'learning_bites'];
-                $data = $this->fetchEventData($events, $properties, $interval, $from);
+                $data = $this->fetchEventData($events, $properties, $interval, $fromPosthog);
+
+                $writingStreak = 0;
+                $writingStreakComplete = true;
+                foreach ($data['events']['check'] as $day => $value) {
+                    // skip everything that isn't start of the week
+                    // @TODO honor the users start of the week 
+                    if ($interval === 'day' && (int)date('w', strtotime($day)) !== 0) {
+                        continue;
+                    }
+
+                    if ((int)$value === 0) {
+                        $writingStreak = 0;
+                        $writingStreakComplete = false;
+                    }
+                    $writingStreak += ($value ? 1 : 0);
+                }
+
+                if ($writingStreakComplete) {
+                    $columnName = $model instanceof Team ? 'team_id' : 'user_id';
+                    $query = Kpi::where($columnName, $model->id)
+                        ->where('kpi', Kpi::WRITING_STREAK)
+                        ->groupBy('year_date', 'week_date')
+                        ->having(DB::raw('SUM(value)'), '=', 0)
+                        ->orderBy('year_date', 'DESC')
+                        ->orderBy('week_date', 'DESC')
+                        ->limit(1)
+                        ->select(DB::raw('YEAR(date) AS year_date'), DB::raw('WEEK(date) AS week_date'));
+
+                    // most recent week before the current writing streak started
+                    $writingStreakEnd = $query
+                        ->first();
+
+                    // user/team has a writing streak since the beginning, ie. they never had a week without a writing streak
+                    if (empty($writingStreakEnd)) {
+                        $query = $model instanceof Team ? Team::query() : User::query();
+                        $query->where('id', $model->id)
+                            ->select(DB::raw('YEAR(created_at) AS year_date'), DB::raw('WEEK(created_at) AS week_date'));
+
+                        $writingStreakEnd = $query
+                            ->first();
+                    }
+
+                    $writingStreakEnd = $writingStreakEnd->toArray();
+                    $writingStreakStart = new DateTime();
+                    $writingStreakStart->setISODate($writingStreakEnd['year_date'], $writingStreakEnd['week_date']);
+                    $writingStreakStart->modify('+7 day');
+
+                    // writing streaks can only have started in December
+                    $earliestWritingStreakStart = new DateTime('2022-12-01');
+
+                    if (empty($writingStreakStart) || $writingStreakStart < $earliestWritingStreakStart) {
+                        $writingStreakStart = $earliestWritingStreakStart;
+                    }
+
+                    $writingStreak = max(
+                        $writingStreak,
+                        $this->numWeeks($writingStreakStart, new DateTime())
+                    );
+                }
+
+                unset($data['events']['check']);
+                $data['writing_streak'] = $writingStreak;
                 break;
             case 'topSubcategories':
                 $events = ['popover_open', 'alternative', 'ignore'];
@@ -275,7 +318,7 @@ class AnalyticsController extends Controller
                 break;
             case 'topWords':
                 $events = ['popover_open', 'alternative', 'ignore'];
-                $data = $this->fetchBreakdown($events, $properties, 'response__data_text', $interval, $from);
+                $data = $this->fetchBreakdown($events, $properties, 'response__data_text', $interval, $fromPosthog);
                 break;
             default:
                 return response()->json(['error' => 400, 'message' => "Unsupported chart type '$chart'"], 400);
@@ -312,8 +355,6 @@ class AnalyticsController extends Controller
 
     protected function fetchFrom(Request $request)
     {
-        $from = max(min((int)$request->get('from', '30'), 30), 1);
-
-        return "-{$from}d";
+        return max(min((int)$request->get('from', '30'), 30), 1);
     }
 }

commit b29e9f18ef6dd0cb18a7d4226f29c8d73677b696

fixed regressions introduced in #380

app/Http/Controllers/AnalyticsController.php
@@ -236,7 +236,7 @@ class AnalyticsController extends Controller
                 $writingStreakComplete = true;
                 foreach ($data['events']['check'] as $day => $value) {
                     // skip everything that isn't start of the week
-                    // @TODO honor the users start of the week 
+                    // @TODO honor the users start of the week
                     if ($interval === 'day' && (int)date('w', strtotime($day)) !== 0) {
                         continue;
                     }
@@ -296,7 +296,7 @@ class AnalyticsController extends Controller
                 break;
             case 'topSubcategories':
                 $events = ['popover_open', 'alternative', 'ignore'];
-                $data = $this->fetchBreakdown($events, $properties, 'response__data__subcategory', $interval, $from);
+                $data = $this->fetchBreakdown($events, $properties, 'response__data__subcategory', $interval, $fromPosthog);
 
                 $locale = session('locale', 'en');
                 foreach ($events as $event) {

resources/views/analytics.blade.php
@@ -10,7 +10,7 @@
                 @include('partials.banners')
                 <div class="ibarra-sub-title-h1 margin-top">
                     {{ __('content.analytics') }}
-                </div>    
+                </div>
                 <div id="lastRefresh" class="lato-small-text-p wittyworks-margin-right container-row margin-top" style="visibility: hidden; align-items: center;"></div>
                 <div class="ibarra-sub-title-h2 wittyworks-margin-top">{{ __('content.activity') }}</div>
                 <div class="wittyworks-form-section container border-radius">
@@ -234,10 +234,10 @@
     const yValuesDauIgnore = [];
     const yValuesDauAlternative = [];
     const yValuesDauPopoverOpen = [];
-    const yValuesDauLearningBites = []; 
+    const yValuesDauLearningBites = [];
 
     const xValuesCheck = [];
-    const xValuesCheckIntervallWeek = [];
+    const xValuesPopoverOpenIntervallWeek = [];
     const yValuesCheck = [];
     const yValuesCheckWeek = [];
 
@@ -343,7 +343,7 @@
             options: {
                 events: [],
                 maintainAspectRatio: false,
-                responsive: true, 
+                responsive: true,
                 title: {
                 display: true,
                 text: text,
@@ -402,7 +402,7 @@
                 options: {
                     legend: {display: false},
                     maintainAspectRatio: false,
-                    responsive: true, 
+                    responsive: true,
                     tooltips: {
                         yAlign: 'bottom',
                         callbacks: {
@@ -415,7 +415,7 @@
                             },
                         },
                         backgroundColor: '#ffffff',
-                        bodyFontColor: '#000000',                    
+                        bodyFontColor: '#000000',
                     },
                     title: {
                         display: true,
@@ -440,9 +440,9 @@
         for (const [event, value] of Object.entries(events)) {
             if (event === 'popover_open') {
                for (const [date, count] of Object.entries(value)) {
-                    xValuesCheck.push(date);
-                    xValuesCheckIntervallWeek.push(moment(date).startOf('week').isoWeekday(startOfWeek).week());
-                    yValuesCheck.push(count);
+                    xValuesPopoverOpen.push(date);
+                    xValuesPopoverOpenIntervallWeek.push(moment(date).startOf('week').isoWeekday(startOfWeek).week());
+                    yValuesPopoverOpen.push(count);
                 }
             } else if (event === 'ignore') {
                 for (const [date, count] of Object.entries(value)) {
@@ -495,7 +495,7 @@
             const changeInIgnorePercentage = (((totalWeeklyIgnore - totalWeeklyIgnorePrevious) / (totalWeeklyIgnorePrevious == 0 ? 1 : totalWeeklyIgnorePrevious)) * 100).toFixed(0).replace('-', '');
             const changeInAlternativePercentage = (((totalWeeklyAlternative - totalWeeklyAlternativePrevious) / (totalWeeklyAlternativePrevious == 0 ? 1 : totalWeeklyAlternativePrevious)) * 100).toFixed(0).replace('-', '');
             
-            const aggregatedCheckChatData =  aggregate_chart_data_by_week(xValuesCheckIntervallWeek, yValuesCheck);
+            const aggregatedCheckChatData =  aggregate_chart_data_by_week(xValuesPopoverOpenIntervallWeek, yValuesCheck);
 
             document.getElementById("checkDaysInRow").innerHTML =  '{{ __('content.writing_streak') }}' + '&nbsp; <span class="lato-small-paragraph-title-h4-purple">' +  writing_streak + '&nbsp</span>' + '{{ __('content.in_a_row') }}';
             document.getElementById("changeInLearningBitesPercentage").innerHTML =  '{{ __('content.you_clicked') }}' + '&nbsp; <span class="lato-small-paragraph-title-h4-purple">' + changeInLearningBitesPercentage + '%</span>&nbsp;' + (changeInLearningBitesPercentage >= 0 ? '{{ __('content.learning_bites_requests_week_positive') }}' : '{{ __('content.learning_bites_requests_week_negative') }}');
@@ -537,69 +537,69 @@
             }, 30000);
             document.getElementById('lastRefresh').style.visibility = 'visible';
 
-            const aggregatedPopoverOpenData = aggregate_chart_data_by_week(xValuesCheckIntervallWeek, yValuesPopoverOpen)[1];
-            const aggregatedAlternativeData = aggregate_chart_data_by_week(xValuesCheckIntervallWeek, yValuesAlternative)[1];
-            const aggregatedIgnoreData = aggregate_chart_data_by_week(xValuesCheckIntervallWeek, yValuesIgnore)[1];
-            const aggregatedLearningBitesData = aggregate_chart_data_by_week(xValuesCheckIntervallWeek, yValuesLearningBites)[1];
+            const aggregatedPopoverOpenData = aggregate_chart_data_by_week(xValuesPopoverOpenIntervallWeek, yValuesPopoverOpen)[1];
+            const aggregatedAlternativeData = aggregate_chart_data_by_week(xValuesPopoverOpenIntervallWeek, yValuesAlternative)[1];
+            const aggregatedIgnoreData = aggregate_chart_data_by_week(xValuesPopoverOpenIntervallWeek, yValuesIgnore)[1];
+            const aggregatedLearningBitesData = aggregate_chart_data_by_week(xValuesPopoverOpenIntervallWeek, yValuesLearningBites)[1];
 
             if(aggregatedLearningBitesData.every(Number.isInteger)) {
                 ctx = document.getElementById('eventsChart').getContext('2d');
-            new Chart(ctx, {
-                type: "line",
-                data: {
-                    labels: aggregatedCheckChatData[0],
-                    datasets: [
-                        {
-                            data: aggregatedPopoverOpenData,
-                            borderColor: colors[3],
-                            fill: false,
-                            label: "{{ __('content.popover_label_line_chart') }}",
-                        },
-                        {
-                            data: aggregatedAlternativeData,
-                            borderColor: colors[6],
-                            fill: false,
-                            label: "{{ __('content.alternative_label_line_chart') }}",
-                        },
-                        {
-                            data: aggregatedIgnoreData,
-                            borderColor: colors[9],
-                            fill: false,
-                            label:  "{{ __('content.ignored_label_line_chart') }}",
-                        },
-                        {
-                            data: aggregatedLearningBitesData,
-                            borderColor: colors[12],
-                            fill: false,
-                            label:  @json(__('content.learning_bites_label_line_chart')),
-                        },
-                    ]
-                },
-                options: {
-                    events: [],
-                    maintainAspectRatio: false,
-                    responsive: true, 
-                    title: {
-                    display: true,
-                    text:  @json(__('content.title_line_chart')),
-                    fontSize: 16,
-                    fontStyle: 'normal'
+                new Chart(ctx, {
+                    type: "line",
+                    data: {
+                        labels: aggregatedCheckChatData[0],
+                        datasets: [
+                            {
+                                data: aggregatedPopoverOpenData,
+                                borderColor: colors[3],
+                                fill: false,
+                                label: "{{ __('content.popover_label_line_chart') }}",
+                            },
+                            {
+                                data: aggregatedAlternativeData,
+                                borderColor: colors[6],
+                                fill: false,
+                                label: "{{ __('content.alternative_label_line_chart') }}",
+                            },
+                            {
+                                data: aggregatedIgnoreData,
+                                borderColor: colors[9],
+                                fill: false,
+                                label:  "{{ __('content.ignored_label_line_chart') }}",
+                            },
+                            {
+                                data: aggregatedLearningBitesData,
+                                borderColor: colors[12],
+                                fill: false,
+                                label:  @json(__('content.learning_bites_label_line_chart')),
+                            },
+                        ]
                     },
-                    scales:{
-                        yAxes: [{
-                            ticks: {
-                                min: 0,
-                                callback: function(value, index, values) {
-                                    if (Math.floor(value) === value) {
-                                        return value;
+                    options: {
+                        events: [],
+                        maintainAspectRatio: false,
+                        responsive: true,
+                        title: {
+                        display: true,
+                        text:  @json(__('content.title_line_chart')),
+                        fontSize: 16,
+                        fontStyle: 'normal'
+                        },
+                        scales:{
+                            yAxes: [{
+                                ticks: {
+                                    min: 0,
+                                    callback: function(value, index, values) {
+                                        if (Math.floor(value) === value) {
+                                            return value;
+                                        }
                                     }
-                                }                        
-                            }
-                        }],
+                                }
+                            }],
+                        }
                     }
-                }
-            });
-        }
+                });
+            }
 
             createBarChart(
                 "eventsLearningBitesChart",
@@ -657,9 +657,9 @@
 
         for (const [event, value] of Object.entries(events)) {
             if (event === 'popover_open') {
-               for (const [date, count] of Object.entries(value)) {
+                for (const [date, count] of Object.entries(value)) {
                     xIntervallDauWeek.push(date);
-                    yValuesDauCheck.push(count);
+                    yValuesDauPopoverOpen.push(count);
                 }
             } else if (event === 'ignore') {
                 for (const [date, count] of Object.entries(value)) {
@@ -669,13 +669,13 @@
                 for (const [date, count] of Object.entries(value)) {
                     yValuesDauAlternative.push(count);
                 }
-            } else if (event === 'user_count') {
-                for (const [date, count] of Object.entries(value)) {
-                    yValuesDauPopoverOpen.push(count);
-                }
             } else if (event === 'learning_bites') {
                 for (const [date, count] of Object.entries(value)) {
                     yValuesDauLearningBites.push(count);
+                }
+            } else if (event === 'user_count') {
+                for (const [date, count] of Object.entries(value)) {
+                    yValuesDauUserCount.push(count);
                 }
             }
         }
@@ -685,7 +685,7 @@
             new Chart(ctx, {
                 type: "line",
                 data: {
-                    labels: aggregatedCheckChatData[0],  //untill posthog offers select start of week
+                    labels: aggregatedCheckChatData[0],  //until posthog offers select start of week
                     datasets: [
                         {
                             data: yValuesDauUserCount,
@@ -722,7 +722,7 @@
                 options: {
                     events: [],
                     maintainAspectRatio: false,
-                    responsive: true, 
+                    responsive: true,
                     animation: {
                         duration: 0
                     },
@@ -740,7 +740,7 @@
                                     if (Math.floor(value) === value) {
                                         return value;
                                     }
-                                }                        
+                                }
                             }
                         }],
                     }
@@ -753,6 +753,7 @@
         });
         }
     });
+
     getChartData('topSubcategories', 7).then(data => {
         if (!data || !data.events ) {
             handle_no_data('topCategoriesChartWrapperNoData', 'loadingIconTopCatagories');
@@ -823,7 +824,7 @@
                     data: dataCategories,
                     options: {
                         maintainAspectRatio: false,
-                        responsive: true, 
+                        responsive: true,
                         events: [],
                         elements: {
                         line: {
@@ -838,7 +839,7 @@
                                         if (Math.floor(value) === value) {
                                             return value;
                                         }
-                                    }                                
+                                    }
                                 }
                             }],
                         }
@@ -1014,7 +1015,7 @@
             options: {
                 events: [],
                 maintainAspectRatio: false,
-                responsive: true, 
+                responsive: true,
                 elements: {
                     line: {
                         borderWidth: 1
@@ -1029,7 +1030,7 @@
                                 if (Math.floor(value) === value) {
                                     return value;
                                 }
-                            }                        
+                            }
                         }
                     }],
                 }
@@ -1098,7 +1099,7 @@
         );
     });
 
-    getChartData('topWords', 30, 'day', 'corporate_rules').then(data => {  
+    getChartData('topWords', 30, 'day', 'corporate_rules').then(data => {
         if((data && data.events && data.events.popover_open)) {
             const openedCorporatewords = data.events.popover_open;
             for (const [key, value] of Object.entries(openedCorporatewords)) {