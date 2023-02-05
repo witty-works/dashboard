53c074917f1fe0abaca482ad3f784548f117d364
Lukas Kahwe Smith <lukas.smith@witty.works>
2023-02-05 13:23:53 +0100

    remove unused translations, updated translations, fixed team show

--- resources/views/teams/show.blade.php
+++ resources/views/teams/show.blade.php
@@ -3,14 +3,16 @@
     <div class="wittyworks-page-wrapper">
         <div class="wittyworks-page lg:ml-20">
             @include('partials.banners', ['hideInviteCheck' => true])
-            <x-slot name="header">
+                <div class="ibarra-sub-title-h1 margin-top">
+                    {{ __('content.show_team') }}
+                </div>
+                <div class="mt-5">
                 @if (!Auth::user()->hasTeamPermission($team, 'update'))
                 {!! __('content.making_changes_requires_admin_rights', ['email' => $team->owner->email]) !!}
                 @endif
-            </x-slot>
 
-            <div>
                 @livewire('teams.team-member-manager-help-hero', ['team' => $team])
+                </div>
             </div>
         </div>
     </div>
