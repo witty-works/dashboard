ee7bc3e3924be3f750b32cff3520cc91904dcaee
Lukas Kahwe Smith <lukas.smith@witty.works>
2023-03-15 23:43:59 +0100

    enable support for posthog apps

--- resources/views/partials/posthog.blade.php
+++ resources/views/partials/posthog.blade.php
@@ -4,6 +4,7 @@
         window.posthog.init(
             {!! json_encode(config('posthog.api_key')) !!}, {
             api_host: {!! json_encode(config('posthog.host'), JSON_UNESCAPED_SLASHES) !!},
+            opt_in_site_apps: true,
             @if (!empty(Auth::user()) || \App\Helpers\PosthogHelper::$posthog_reset)
             loaded: function(posthog) {
                 @if (\App\Helpers\PosthogHelper::$posthog_reset)
