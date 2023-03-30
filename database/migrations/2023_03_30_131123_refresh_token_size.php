12c53d60fec6c34662f585883676513ace0c6ad4
Lukas Kahwe Smith <lukas.smith@witty.works>
2023-03-30 15:14:09 +0200

    increase token column size DASHBOARD-HV

---
+++ database/migrations/2023_03_30_131123_refresh_token_size.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('connected_accounts', function ($table) {
+            $table->string('token', 4000)->change();
+            $table->string('refresh_token', 4000)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('connected_accounts', function ($table) {
+            $table->string('token', 2000)->change();
+            $table->string('refresh_token', 2000)->change();
+        });
+    }
+};
