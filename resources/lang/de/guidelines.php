452e508156c078aff3500f70e9cc8a45a4f418ef
Lukas Kahwe Smith <lukas.smith@witty.works>
2023-05-16 15:42:52 +0200

    updated translations

--- resources/lang/de/guidelines.php
+++ resources/lang/de/guidelines.php
@@ -129,5 +129,5 @@ Alle Wörter, die zu dieser Liste hinzufügst, werden von Witty hervorgehoben, u
 </div>
 So kannst du Witty genau auf deine Bedürfnisse und Prioritäten abstimmen. <span style="color:#f06464">Wir empfehlen, zu Beginn die Stufe <i>Basic</i> zu wählen.</span>',
     'advanced_toggle_example' => 'Um zu verstehen, was das bedeutet, lass uns einen Blick auf die Kategorie <a href="https://www.witty.works/de/kategorien/f%C3%A4higkeiten-erkrankungen-k%C3%B6rperlichkeit/seh-f%C3%A4higkeit" target="_blank">Vision</a> werfen. Ist <i>Basic</i> ausgewählt, würde der Satz <i>"der Einäugige unter den Blinden"</i> hervorgehoben werden. Wenn <i>fortgeschritten</i> eingeschaltet ist, werden auch subtile unbewusste Vorurteile wie der Satz <i>”Wir sehen uns später"</i> hervorgehoben.',
-    'toggle_advanced_vs' => 'Basic vs. Fortgeschritten'
+    'toggle_advanced_vs' => 'Basic vs. Fortgeschritten',
 ];
