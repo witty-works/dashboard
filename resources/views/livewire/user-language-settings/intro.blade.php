27958a7ef90747ec25d0b24ff4d36931c0b0d781
Lukas Kahwe Smith <lukas.smith@witty.works>
2023-05-24 16:23:39 +0200

    add white background to info sections

--- resources/views/livewire/user-language-settings/intro.blade.php
+++ resources/views/livewire/user-language-settings/intro.blade.php
@@ -6,11 +6,9 @@
         </x-slot>
 
         <x-slot name="description" class="lato-paragraph-text-p">
-            <div>
+            <div class="container border-radius">
                 {!! __('guidelines.reset_to_team_suggestions_description') !!}
-            </div>
 
-            <div>
                 <form action="{{ route('user.language-settings-reset') }}">
                 <x-jet-button>
                     {{ __('guidelines.reset_to_team') }}
