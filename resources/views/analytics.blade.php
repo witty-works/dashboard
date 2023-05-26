2326f6756b5818d725b67ec1f689ec48ec9769a4
Lukas Kahwe Smith <lukas.smith@witty.works>
2023-05-26 09:08:55 +0200

    translate tab titles

--- resources/views/analytics.blade.php
+++ resources/views/analytics.blade.php
@@ -14,15 +14,15 @@
       <div id="lastRefresh" class="lato-small-text-p wittyworks-margin-right container-row margin-top" style="visibility: hidden; align-items: center;"></div>
       <div class="analytics-tab-container">
          <div class="analytics-tab" id="overview-tab" onclick="handleTabClick('overview')" style="color: #9489DB;">
-            Overview
+            {{ __('content.analytic_overview_tab_title') }}
             <div class="analytics-tab-line" id="overview-line"></div>
         </div>
          <div class="analytics-tab" id="top-categories-tab" onclick="handleTabClick('top-categories')">
-            Top Categories
+            {{ __('content.analytic_top_categories_tab_title') }}
             <div id="top-categories-line"></div>
         </div>
          <div class="analytics-tab" id="top-words-tab" onclick="handleTabClick('top-words')">
-            Top Words
+            {{ __('content.analytic_top_words_tab_title') }}
             <div id="top-words-line"></div>
         </div>
       </div>
