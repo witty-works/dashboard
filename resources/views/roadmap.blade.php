e71db76f5ea2bb03e0cb57d489e0fd83a16abd90
Lukas Kahwe Smith <lukas.smith@witty.works>
2023-06-02 17:18:58 +0200

    add debug information for productboard token

--- resources/views/roadmap.blade.php
+++ resources/views/roadmap.blade.php
@@ -2,6 +2,8 @@
     <div class="wittyworks-navigation-wrapper">@livewire('navigation-menu')</div>
         <div class="wittyworks-page-wrapper">
             <div class="wittyworks-page lg:ml-20">
+                token: {{ $token }}<br/>
+                decoded: {!! print_r((array)\Firebase\JWT\JWT::decode($token, new \Firebase\JWT\Key(config('productboard.private_key'), 'HS256')), true) !!}
                 <iframe src="{{ $url }}?hide_logo=1&token={{ urlencode($token) }}" frameborder="0" class="h-screen w-full" title="{{ __('content.roadmap') }}"></iframe>
             </div>
         </div>
