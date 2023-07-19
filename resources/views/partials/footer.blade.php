ae3d7a330884874379dd8aa567006c8cc4901537
Lukas Kahwe Smith <lukas.smith@witty.works>
2023-07-19 10:04:03 +0200

    fix contact link

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -8,7 +8,7 @@ $resourceLinks = [
 ];
 
 $contactLinks = [
-    __('content.contact') => 'https://www.witty.works/contact-sales',
+    __('content.contact') => 'https://www.witty.works/contact',
     __('content.book-demo') => 'https://www.witty.works/demo',
     __('content.help') => 'https://www.witty.works/en/help/wittys-help-center',
 ];
