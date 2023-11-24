3ee8929b9990bf2e87de6230a88c2c66755e422d
Lukas Kahwe Smith <lukas.smith@witty.works>
2023-11-24 11:43:43 +0100

    disable stateful middleware and CSRF token for API requests on /refresh-token and /user/language/domains

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,8 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        'stripe/*'
+        'stripe/*',
+        '/refresh-token',
+        '/user/language/domains',
     ];
 }
