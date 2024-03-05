2f31848b15aad33e69f61c3b639b9dd1cb6ffb32
Lukas Kahwe Smith <lukas.smith@witty.works>
2024-03-05 13:54:18 +0100

    fix name updating

--- resources/views/profile/update-profile-information-form.blade.php
+++ resources/views/profile/update-profile-information-form.blade.php
@@ -13,7 +13,7 @@
 
             @if (Session::get('login_source') !== \App\Http\Controllers\OAuthController::OFFICE_PROVIDER)
             <a href="{{ route('oauth.redirect', ['provider' => 'azureadb2c', 'policy' => 'profile']) }}">
-                {!! __('content.update_your_account_profile', ['profile_url' => '']) !!}
+                {!! __('content.update_your_account_profile', ['profile_url' => route('oauth.redirect', ['provider' => 'azureadb2c', 'policy' => 'profile'])]) !!}
             </a>
             @endif
         </div>
