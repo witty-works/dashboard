5f6c897366d472ea5a4ad6db2f5a8439114a4251
Lukas Kahwe Smith <lukas.smith@witty.works>
2024-03-05 14:29:29 +0100

    fix label escaping

--- resources/views/teams/language-settings.blade.php
+++ resources/views/teams/language-settings.blade.php
@@ -1,6 +1,6 @@
 <div>
     <div class="ibarra-sub-title-h1 margin-top margin-bottom">
-        {{ __('guidelines.language_settings_label') }}
+        {!! __('guidelines.language_settings_label') !!}
     </div>
 
     @livewire('organization-language-settings.language', ['model' => $team])
