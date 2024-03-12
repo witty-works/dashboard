fe1c96d1722b273f390268824cc038f1fd54e24d
Lukas Kahwe Smith <lukas.smith@witty.works>
2024-03-12 14:31:55 +0100

    fix typos in slack events

--- app/Listeners/TeamMemberAddedSlackAlert.php
+++ app/Listeners/TeamMemberAddedSlackAlert.php
@@ -10,7 +10,7 @@ class TeamMemberAddedSlackAlert
     {
         $subscription = $event->team->subscription();
         if (!$subscription) {
-            $message = " - A new team member '{$event->user->email}' was added to '{$event->team->name}' ('{$event->team->owner->email}'), total count is now at {$team->getTotalUserCount()} via {$this->source}";
+            $message = " - A new team member '{$event->user->email}' was added to '{$event->team->name}' ('{$event->team->owner->email}'), total count is now at {$event->team->getTotalUserCount()} via {$event->source}";
 
             SlackAlert::message(getenv('PLATFORM_ENVIRONMENT') . $message);
         }
--- app/Listeners/TeamMemberInvitedSlackAlert.php
+++ app/Listeners/TeamMemberInvitedSlackAlert.php
@@ -10,7 +10,7 @@ class TeamMemberInvitedSlackAlert
     {
         $subscription = $event->team->subscription();
         if (!$subscription) {
-            $message = " - A new team member '{$event->email}' was invited to '{$event->team->name}' ('{$event->team->owner->email}'), total invited users at {$$event->team->teamInvitations()->count()} via {$event->source}";
+            $message = " - A new team member '{$event->email}' was invited to '{$event->team->name}' ('{$event->team->owner->email}'), total invited users at {$event->team->teamInvitations()->count()} via {$event->source}";
 
             SlackAlert::message(getenv('PLATFORM_ENVIRONMENT') . $message);
         }
