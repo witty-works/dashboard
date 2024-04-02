b6d429b324d24bbba61c5775585b58ac1469ec38
Lukas Kahwe Smith <lukas.smith@witty.works>
2024-04-02 13:56:43 +0200

    persist original login source

--- app/Listeners/UserAddedSlackAlert.php
+++ app/Listeners/UserAddedSlackAlert.php
@@ -12,7 +12,7 @@ class UserAddedSlackAlert
         $message = sprintf(
             " - A new user was added %s via %s",
             $event->user->email,
-            $event->user->getSignupSource(),
+            $event->user->source,
         );
 
         SlackAlert::message(getenv('PLATFORM_ENVIRONMENT') . $message);
