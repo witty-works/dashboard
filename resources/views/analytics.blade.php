7cb679a28eb995af45d69e7758bb724ec754cfec
Solveig Helland <hi@solveig.io>
2024-04-02 09:28:23 +0200

    fix: when no change show 0% change not 100%

--- resources/views/analytics.blade.php
+++ resources/views/analytics.blade.php
@@ -461,7 +461,7 @@
                                     if (previousPeriodPopoverOpened[currentLabel]) {
                                         const diff = currentValue - previousPeriodPopoverOpened[currentLabel];
                                         const percentage = (diff / previousPeriodPopoverOpened[currentLabel] * 100).toFixed(0);
-                                        if (percentage == 0) return '+ 100 %';
+                                        if (percentage == 0)  return '+ 0 %';
                                         return percentage >= 0 ? '+' + percentage + '%' : percentage + '%';
                                     } else {
                                         return '+ 100 %';
