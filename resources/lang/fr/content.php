6db88643a185e538c64e7d9642fde6c08fa45a87
Lukas Kahwe Smith <lukas.smith@witty.works>
2024-05-16 16:15:34 +0200

    tweak french example

--- resources/lang/fr/content.php
+++ resources/lang/fr/content.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'witty_editor_example_text' => '<h2>L\'essentiel</h2><h4>Les mots d\'esprit contenant des préjugés inconscients sont en orange:</h4><br><br><em>Salut les gars.<br>Nous sommes une équipe ambitieuse. C\'est pourquoi nous avons besoin de personnes très performantes.</em><br><br><h4>En vert, les termes positifs sont soulignés avec esprit :</h4><br><em>Nous sommes une équipe créative.</em><br><br><h4>Witty souligne les fautes de grammaire et d\'orthographe en rouge:</h4><br><em>Il s\'agit d\'une faute d\'orthographie.</em>
+    'witty_editor_example_text' => '<h2>L\'essentiel</h2><h4>Les mots d\'esprit contenant des préjugés inconscients sont en orange :</h4><br><br><em>Salut les gars.<br>Nous sommes une équipe ambitieuse. C\'est pourquoi nous avons besoin de personnes très performantes.</em><br><br><h4>En vert, les termes positifs sont soulignés avec esprit :</h4><br><em>Nous sommes une équipe créative.</em><br><br><h4>Witty souligne les fautes de grammaire et d\'orthographe en rouge :</h4><br><em>Il s\'agit d\'une faute d\'orthographie.</em>
 
 <h2>Où fonctionne Witty</h2>
 <p>Witty fonctionne sur la <a href="https://www.witty.works/witty-works-where-you-do" rel="noopener" target="_blank" style="color: rgb(85, 184, 233);">plupart des pages web</a> où vous écrivez. Witty est activé automatiquement. Pour Microsoft Word, utilisez le complément <a href="https://appsource.microsoft.com/fr-fr/product/office/wa200006592?tab=overview" rel="noopener" target="_blank" style="color: rgb(85, 184, 233);">Witty pour Word</a>. Si vous utilisez des d\'autres produits Microsoft, utilisez plutôt notre <a href="http://dashboard.witty.works/editor" rel="noopener" target="_blank" style="color: rgb(85, 184, 233);">éditeur</a>.</p>
