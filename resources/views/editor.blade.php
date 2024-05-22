7f400ea0752d58e2e5f23f2f0ef5df41a6ab1a5c
Lukas Kahwe Smith <lukas.smith@witty.works>
2024-05-22 14:27:50 +0200

    replace fullscreen icon with witty logo

--- resources/views/editor.blade.php
+++ resources/views/editor.blade.php
@@ -83,6 +83,20 @@
                                     editorElement.dispatchEvent(event);
                                 }
                             });
+                            FroalaEditor.DefineIcon('dashboard', {
+                                template: 'image',
+                                SRC: @json(URL::asset('witty-icon-color-inverted@2x-1.webp')),
+                                ALT: 'Dashboard'
+                            });
+                            FroalaEditor.RegisterCommand('dashboard', {
+                                title: 'Dashboard',
+                                focus: false,
+                                undo: false,
+                                refreshAfterCallback: false,
+                                callback: function () {
+                                    editor.fullscreen.toggle();
+                                }
+                            });
 
                             editor = new FroalaEditor('#witty_editor', {
                                 key: @json(config('app.froala_key')),
@@ -92,10 +106,10 @@
                                 spellcheck: false,
                                 heightMax: 800,
                                 enter: FroalaEditor.ENTER_BR,
-                                toolbarButtons: ['fullscreen', 'bold', 'italic', 'underline', 'strikeThrough', 'fontSize', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'insertLink', 'clearFormatting', 'undo', 'redo', 'copy', 'example', 'help'],
-                                toolbarButtonsMD: ['fullscreen', 'bold', 'italic', 'underline', 'strikeThrough', 'fontSize', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'insertLink', 'clearFormatting', 'undo', 'redo', 'copy', 'example', 'help'],
-                                toolbarButtonsSM: ['fullscreen', 'bold', 'underline', 'strikeThrough', 'fontSize', 'insertLink', 'undo', 'redo', 'copy', 'example', 'help'],
-                                toolbarButtonsXS: ['fullscreen', 'bold', 'underline', 'fontSize', 'copy', 'help'],
+                                toolbarButtons: ['dashboard', 'bold', 'italic', 'underline', 'strikeThrough', 'fontSize', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'insertLink', 'clearFormatting', 'undo', 'redo', 'copy', 'example', 'help'],
+                                toolbarButtonsMD: ['dashboard', 'bold', 'italic', 'underline', 'strikeThrough', 'fontSize', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'insertLink', 'clearFormatting', 'undo', 'redo', 'copy', 'example', 'help'],
+                                toolbarButtonsSM: ['dashboard', 'bold', 'underline', 'strikeThrough', 'fontSize', 'insertLink', 'undo', 'redo', 'copy', 'example', 'help'],
+                                toolbarButtonsXS: ['dashboard', 'bold', 'underline', 'fontSize', 'copy', 'help'],
                             }, function () {
                             @if(!request()->get('onboarding'))
                                 editor.fullscreen.toggle();
