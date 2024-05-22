7bb6c2953714c7fce713af6773d47927a6b48172
Lukas Kahwe Smith <lukas.smith@witty.works>
2024-05-22 14:44:12 +0200

    translation update

--- resources/lang/en/content.php
+++ resources/lang/en/content.php
@@ -363,7 +363,7 @@ If you have any questions, contact :name (:email). 🙂
     'invitation_request_license_limit_reached' => 'You have already invited two members to your team and have reached the limit. <a href="https://dashboard.witty.works/team/subscription">Upgrade</a> to invite more members!',
     'witty_editor_copy_button' => 'Copy text to clipboard',
     'witty_editor_example_button' => 'Show Example',
-    'witty_editor_example_text' => '<h4>Witty highlights language containing unconscious biased in orange and yellow.</h4><br><br><em>Hey guys,<br>We have a large software project that will take man-hours to complete. Who would like to join us? Bring your extraordinary expertise and strong technical background. We are also looking for a digital native.</em><br><br><em>P.S. Your wives are welcome at the board\'s Christmas party.</em><br><br><h4>Witty highlights positive terms in green.</h4><br><em>We are a creative team.</em><br><h4>Witty highlights grammar and spelling mistakes in red.</h4><br><em>This is a spelling mistacke.</em>',
+    'witty_editor_example_text' => '<h4>Witty highlights language containing unconscious biased in orange and yellow.</h4><br><h3>Hey guys,<br>We have a large software project that will take man-hours to complete. Who would like to join us? Bring your extraordinary expertise and strong technical background. We are also looking for a digital native.</h3><h3><br></h3><h3>P.S. Your wives are welcome at the board\'s Christmas party.</h3><br><h4>Witty highlights positive terms in green.</h4><br><h3>We are a creative team.</h3>​<h4>Witty highlights grammar and spelling mistakes in red.</h4><br><h3>This is a spelling mistacke.</h3>',
     'learn_more' => 'Dig deeper',
     'example' => 'Example',
     'open' => 'Open',
