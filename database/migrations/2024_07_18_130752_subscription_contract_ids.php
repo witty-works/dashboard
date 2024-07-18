de76da3ad281ad019c0966e40a64d7dd077309f8
Lukas Kahwe Smith <lukas.smith@witty.works>
2024-07-18 15:09:51 +0200

    add fields for the witty contract id and company name

---
+++ database/migrations/2024_07_18_130752_subscription_contract_ids.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('subscriptions', function (Blueprint $table) {
+            $table->string('witty_contract_id')->nullable();
+            $table->string('company_name')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('subscriptions', function (Blueprint $table) {
+            $table->dropColumn('witty_contract_id');
+            $table->dropColumn('company_name');
+        });
+    }
+};
