b90e1f09b72cc0e5dec3ea93a4ed84db935ceeb9
Lukas Kahwe Smith <lukas.smith@witty.works>
2024-08-14 12:00:03 +0200

    remove useless whitespace on browser-login page

--- resources/views/browser-login.blade.php
+++ resources/views/browser-login.blade.php
@@ -3,25 +3,21 @@
         <h2>{{ __('content.browser_login') }}</h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                @auth
-                    @if(env('APP_DEBUG'))
-                    <ul>
-                        <li>email: {{ $email }}</li>
-                        <li>access_token: {{ $access_token }}</li>
-                        <li>refresh_token: {{ $refresh_token }}</li>
-                    </ul>
-                    @else
-                    {{ __('content.login_failed') }}
-                    @endif
-                @else
-                <a href="{{ route('browser_login') }}">
-                    {{ __('content.log_in') }}
-                </a>
-                @endauth
-            </div>
-        </div>
+    <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+        @auth
+            @if(env('APP_DEBUG'))
+            <ul>
+                <li>email: {{ $email }}</li>
+                <li>access_token: {{ $access_token }}</li>
+                <li>refresh_token: {{ $refresh_token }}</li>
+            </ul>
+            @else
+            {{ __('content.login_failed') }}
+            @endif
+        @else
+        <a href="{{ route('browser_login') }}">
+            {{ __('content.log_in') }}
+        </a>
+        @endauth
     </div>
 </x-app-layout>
