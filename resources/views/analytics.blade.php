874889b429b42497c225423f4618342593de237e
Lukas Kahwe Smith <lukas.smith@witty.works>
2024-08-26 09:17:10 +0200

    fixed string encoding

--- resources/views/analytics.blade.php
+++ resources/views/analytics.blade.php
@@ -346,9 +346,9 @@
         const textElement = textElementId ? document.getElementById(textElementId) : null;
         if (textElement) {
             if (isApiError) {
-                textElement.innerHTML = '{!! __('content.no_data_image_text_temp_unavailable') !!}';
+                textElement.innerHTML = @json('content.no_data_image_text_temp_unavailable');
             } else {
-                textElement.innerHTML = '{!! __('content.no_data_image_text') !!}';
+                textElement.innerHTML = @json('content.no_data_image_text');
             }
         }
     }
