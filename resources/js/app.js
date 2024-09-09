c5a9a65072a742396bfc6b7e113225fd7d5c25f1
Lukas Kahwe Smith <lukas.smith@witty.works>
2024-09-09 13:21:36 +0200

    remove sentry tracing

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,9 +6,6 @@ if (window.sentry_dsn) {
     Sentry.init({
         dsn: window.sentry_dsn,
         release: window.app_name + "@" + window.sentry_release,
-        integrations: [
-            new Sentry.browserTracingIntegration(),
-        ],
         tracesSampleRate: window.traces_sample_rate,
         tracingOptions: {
             trackComponents: true,
