78b28268a9bfb476b14c9b7040b2865a7fa9c0e1
Lukas Kahwe Smith <lukas.smith@witty.works>
2024-10-24 17:22:31 +0200

    updated links for explanation on inclusive nouns

--- resources/lang/en/guidelines.php
+++ resources/lang/en/guidelines.php
@@ -135,7 +135,7 @@ All words added to this list will become highlighted by Witty, and your custom r
     'french_only' => 'French only',
     'other_language' => 'Only in other languages',
     'manage_organization_guidelines_generic_masculine' => 'Generic masculine in gendered languages',
-    'manage_organization_guidelines_description_generic_masculine' => 'How should Witty handle the generic masculine in gendered languages like German or French?',
+    'manage_organization_guidelines_description_generic_masculine' => 'How should Witty handle the generic masculine in gendered languages like <a href="https://www.witty.works/en/blog/gender-inclusive-writing-in-german" target="_new">German</a> or <a href="https://www.witty.works/fr/blog/post/langage-non-genr%C3%A9-en-fran%C3%A7ais" target="_new">French</a>?',
     'llm_alternatives_headline' => 'Grammatically correct alternatives (coming in Q4)',
     'llm_alternatives_description' => 'If Witty should use a Large-Language-Model (LLM) to generate grammatically correct alternatives. Witty uses prompts that restrict the freedom of the model to minimize risks of biases. No data is stored or used for training purposes related to this feature as explained on our <a href="https://www.witty.works/trust-and-security">Trust & Security</a> page. If this feature is disabled users will need to manually adjust alternatives to fit.',
     'llm_alternatives' => 'Use LLM for alternatives',
